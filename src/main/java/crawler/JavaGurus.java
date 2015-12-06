5980135911c4dd8ed7082e752a3fe81292fd3b1c
Debmalya Jash <debmalya.jash@gmail.com>
2015-12-06 13:47:50 +0800

    Only priting name of Java Gurus

--- src/main/java/crawler/JavaGurus.java
+++ src/main/java/crawler/JavaGurus.java
@@ -4,7 +4,8 @@
 package crawler;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -17,27 +18,38 @@ import org.jsoup.select.Elements;
  */
 public class JavaGurus {
 
+	private static Set<String> gurus = new HashSet<>();
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		boolean isRepeated = false;
 		try {
-			Document document = Jsoup.connect("https://java.net/website/java-champions/bios.html").timeout(0).get();
+			Document document = Jsoup
+					.connect(
+							"https://java.net/website/java-champions/bios.html")
+					.timeout(0).get();
 			Elements elements = document.getElementsByTag("a");
-			for (Element element:elements) {
-				System.out.println(element.ownText());
+			for (Element element : elements) {
+				
+				if (gurus.add(element.ownText()) && !isRepeated) {					
+					System.out.println(element.ownText());
+				} else {					
+					isRepeated = true;
+				}
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		
 	}
-	
+
 	/**
 	 * 
-	 * @param document complete document
+	 * @param document
+	 *            complete document
 	 * @return
 	 */
 	public Elements getAllElements(Document document) {
