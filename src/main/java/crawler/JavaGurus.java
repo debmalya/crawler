64846db9b7e211988981b3f57482bbf4f923458b
Debmalya Jash <debmalya.jash@gmail.com>
2015-12-31 00:19:14 +0800

    To make code more green and clean

--- src/main/java/crawler/JavaGurus.java
+++ src/main/java/crawler/JavaGurus.java
@@ -44,19 +44,9 @@ public class JavaGurus {
 			for (Element element : elements) {
 				
 				if (gurus.add(element.ownText()) && !isRepeated) {					
-//					System.out.println(element.ownText());
-//					System.out.println(element.attr("href"));
-//					System.out.println(document.getElementsContainingOwnText(element.attr("href")));
-//					From the link get details of gurus.
-					
 					aboutThem.put(element.ownText(), null);
 				} else {					
 					isRepeated = true;
-//					/html/body/p[2]/table/tbody/tr[2]/td/p[2]
-//					what text comes after this is about java guru.
-//					Repeated - here will come the details of guru.
-//					Get the details.
-//					System.out.println(element.getElementsByAttribute("href"));
 					System.out.println(element.ownText()+ " " + element.nextSibling());
 				}
 			}
